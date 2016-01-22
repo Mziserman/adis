d33e857dc875789a0cdd14a0a5bd620cb6dba386
Mziserman <martinziserman@gmail.com>
2016-01-22 13:50:36 +0100

    added text

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 
 <div id="main">
 	<div id="content">
-
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			<?php get_template_part('content'); ?>
 		<?php endwhile; else: ?>
--- offre.php
+++ offre.php
@@ -1,8 +1,8 @@
 <div class="thumbnail-img"><?php the_post_thumbnail() ?></div>
 <?php $post_id = get_the_ID() ?>
-<h1><?php the_title(); ?></h1>
-<small>Posted on: <?php the_time('F j, Y'); ?> at <?php the_time('g:i a'); ?></small>
-<p><?php the_content(); ?></p>
+<h1>Intitulé du poste : <?php the_title(); ?></h1>
+<small>Posté le <?php the_time('j F Y'); ?> à <?php the_time('g:i a'); ?></small>
+<p>Description du poste : <?php the_content(); ?></p>
 <?php 
 	$field = get_fields($post_id); 
 	echo '<pre>';
